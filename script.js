72f38d72bbcf50dbd4fabe043d04a097fb5796ce
Evon <fumblies@gmail.com>
2016-09-04 23:36:14 +0800

    background color changes on scroll

--- script.js
+++ script.js
@@ -12,3 +12,46 @@ $(function () {
     }
   })
 })
+
+// $(document).ready(function () {
+//   $(window).scroll(function () {
+//     if ($(document).scrollTop() > 200) {
+//       $('body').addClass('green')
+//     } else {
+//       $('body').removeClass('green')
+//     }
+//   })
+// })
+
+// // For elements floating into place on scroll
+// $(document).ready(function () {
+//   $(window).scroll(function () {
+//     var wScroll = $(this).scrollTop()
+//
+//     if (wScroll > $('#events').offset().top - $(window).height()) {
+//       var offset = Math.min(0, wScroll - $('#events').offset().top + $(window).height() - 0)
+//       $('body').addClass('green')
+//     } else {
+//       $('body').removeClass('green')
+//     }
+//   })
+// })
+
+$(window).on('scroll touchmove', function () {
+  console.log('whodis')
+  if ($(document).scrollTop() >= $('#header').position().top) {
+    $('body').css('background', $('#header').attr('data-color'))
+  }
+  if ($(document).scrollTop() >= $('#events').position().top - 100) {
+    $('body').css('background', $('#events').attr('data-color'))
+  }
+  if ($(document).scrollTop() > $('#about').position().top - 100) {
+    $('body').css('background', $('#about').attr('data-color'))
+  }
+  if ($(document).scrollTop() > $('#approach').position().top - 300) {
+    $('body').css('background', $('#approach').attr('data-color'))
+  }
+  if ($(document).scrollTop() > $('#involve').position().top - 300) {
+    $('body').css('background', $('#involve').attr('data-color'))
+  }
+})
